d9d8d6122a662d05f279011902e0db9fad207f66
Rick Wang <lurker2000wxc@gmail.com>
2019-07-01 06:30:36 +1000

    eslint files

--- cli-scripts/run-pull-events.js
+++ cli-scripts/run-pull-events.js
@@ -1,16 +1,17 @@
-const chargebee = require("../src/chargebee-obj");
+const chargebee = require('../src/chargebee-obj');
 const pullEvents = require('../src/pull-via-events-api');
 
-;(async()=>{
-  let x = pullEvents(chargebee, {
-    limit : 2,
-    "sort_by[asc]" : "occurred_at",
-    "occurred_at[after]" : 0
-  });
+(async () => {
+  const q = {
+    limit: 2,
+    'sort_by[asc]': 'occurred_at',
+    'occurred_at[after]': 0,
+  };
+  const x = pullEvents(chargebee, q);
 
   let data;
-  do{
+  do {
     data = await x.next();
-    console.log(data)
-  }while(!data.done)
-})()
+    console.log(data);
+  } while (!data.done);
+})();
--- src/pull-via-events-api.js
+++ src/pull-via-events-api.js
@@ -1,15 +1,17 @@
-async function *eventsPull(chargeBeeObj, query = {limit : 1, "sort_by[asc]" : "occurred_at", "occurred_at[after]" : 0}){
-	do{
-		let data = await chargeBeeObj.event.list(query).request();
-		if(!data.next_offset){
-			return data;
-		}
-		query = {
-			...query,
-			offset: data.next_offset
-		}
+async function* eventsPull(chargeBeeObj, argQuery = { limit: 1, 'sort_by[asc]': 'occurred_at', 'occurred_at[after]': 0 }) {
+  let query = { ...argQuery };
 
-		yield data
-	}while(true)
+  do {
+    const data = await chargeBeeObj.event.list(query).request();
+    if (!data.next_offset) {
+      return data;
+    }
+    query = {
+      ...query,
+      offset: data.next_offset,
+    };
+
+    yield data;
+  } while (true);
 }
-module.exports = eventsPull
+module.exports = eventsPull;
